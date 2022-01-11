f130128a8bb2c918c2be71db4c34414a01a844c0
urdeaths <96553932+urdeaths@users.noreply.github.com>
2022-01-11 19:36:10 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,57 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Deathh's World" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <html>
+<head>
+<style>
+.button {
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.button1 {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #4CAF50;
+}
+
+.button1:hover {
+  background-color: #4CAF50;
+  color: white;
+}
+
+.button2 {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #008CBA;
+}
+
+.button2:hover {
+  background-color: #008CBA;
+  color: white;
+}
+
+</style>
+</head>
+<body>
+
+<h1>The button element - Styled with CSS</h1>
+
+<button class="button button1">Green</button>
+<button class="button button2">Blue</button>
+
+</body>
+</html></code>
         </p>
       </main>
 
